29d82630f41dc7542b58eae8afb4b0b3e4c0db7b
OlfatProgressus <olfat@progressus.io>
2025-12-02 11:14:51 +0200

    add logo converter class

--- src/admin/class-widget-handler-factory.php
+++ src/admin/class-widget-handler-factory.php
@@ -38,6 +38,7 @@ class Widget_Handler_Factory {
 		'image-gallery'    => 'Progressus\Gutenberg\Admin\Widget\Gallery_Widget_Handler',
 		'divider'          => 'Progressus\Gutenberg\Admin\Widget\Divider_Widget_Handler',
 		'tabs'             => 'Progressus\Gutenberg\Admin\Widget\Tabs_Widget_Handler',
+		'theme-site-logo'  => 'Progressus\Gutenberg\Admin\Widget\Site_Logo_Widget_Handler',
 	);
 
 	/**
