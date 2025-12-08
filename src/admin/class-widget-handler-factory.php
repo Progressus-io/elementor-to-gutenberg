642f54e01b1d5bb676bf16d11fdc25d36f054e86
OlfatProgressus <olfat@progressus.io>
2025-12-08 15:06:32 +0200

    add form custom block and its converter class

--- src/admin/class-widget-handler-factory.php
+++ src/admin/class-widget-handler-factory.php
@@ -38,6 +38,7 @@ class Widget_Handler_Factory {
 		'image-gallery'    => 'Progressus\Gutenberg\Admin\Widget\Gallery_Widget_Handler',
 		'divider'          => 'Progressus\Gutenberg\Admin\Widget\Divider_Widget_Handler',
 		'tabs'             => 'Progressus\Gutenberg\Admin\Widget\Tabs_Widget_Handler',
+		'testimonial-carousel' => 'Progressus\Gutenberg\Admin\Widget\Testimonial_Carousel_Widget_Handler',
 	);
 
 	/**
