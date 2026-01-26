ebb3d0518f484fe7f5100e0606510a0f2e588654
sal4sup <sal4sup@gmail.com>
2026-01-26 20:30:48 +0300

    refactor woocomerce

---
+++ src/admin/widget/trait-woo-block-serializer.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Progressus\Gutenberg\Admin\Widget;
+
+use function sanitize_key;
+use function wp_json_encode;
+
+defined( 'ABSPATH' ) || exit;
+
+trait Woo_Block_Serializer_Trait {
+	private function is_block_registered( string $block_name ): bool {
+		if ( ! class_exists( 'WP_Block_Type_Registry' ) ) {
+			return false;
+		}
+
+		$block_type = \WP_Block_Type_Registry::get_instance()->get_registered( $block_name );
+		return is_object( $block_type );
+	}
+
+	private function serialize_block( string $block_name, array $attrs, string $inner_html ): string {
+		$parsed = array(
+			'blockName'    => $block_name,
+			'attrs'        => $attrs,
+			'innerBlocks'  => array(),
+			'innerHTML'    => $inner_html,
+			'innerContent' => '' === $inner_html ? array() : array( $inner_html ),
+		);
+
+		if ( function_exists( 'serialize_block' ) ) {
+			return serialize_block( $parsed ) . "\n";
+		}
+
+		$attr_json = empty( $attrs ) ? '' : ' ' . wp_json_encode( $attrs, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE );
+
+		if ( '' === $inner_html ) {
+			return sprintf( '<!-- wp:%s%s /-->%s', sanitize_key( $block_name ), $attr_json, "\n" );
+		}
+
+		return sprintf(
+			"<!-- wp:%s%s -->\n%s\n<!-- /wp:%s -->\n",
+			sanitize_key( $block_name ),
+			$attr_json,
+			$inner_html,
+			sanitize_key( $block_name )
+		);
+	}
+
+	private function serialize_block_or_shortcode( string $preferred_block, string $fallback_shortcode, array $attrs = array() ): string {
+		if ( $this->is_block_registered( $preferred_block ) ) {
+			return $this->serialize_block( $preferred_block, $attrs, '' );
+		}
+
+		return $this->serialize_block( 'core/shortcode', array(), $fallback_shortcode );
+	}
+}
